a7af740fc6901c6fafd0d3af4bb67834aafb3e31
ge36wy <sejoklee@web.de>
2024-12-01 06:15:57 +0100

    Day 1 Part 1

--- AoCTag1/src/Main.java
+++ AoCTag1/src/Main.java
@@ -11,13 +11,26 @@ public class Main {
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line;
             int total = 0;
-            ArrayList<Integer> numbers = new ArrayList<>();
+            ArrayList<Integer> left = new ArrayList<>();
+            ArrayList<Integer> right = new ArrayList<>();
             while ((line = reader.readLine()) != null) {
-
+                String[] split = line.split("   ");
+                left.add(Integer.parseInt(split[0]));
+                right.add(Integer.parseInt(split[1]));
+            }
+            Collections.sort(left);
+            Collections.sort(right);
+            for (int i = 0; i < left.size(); i++){
+                total += abs(left.get(i), right.get(i));
             }
             System.out.println(total);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static int abs(int a, int b){
+        if (a > b) return a - b;
+        return b - a;
     }
 }

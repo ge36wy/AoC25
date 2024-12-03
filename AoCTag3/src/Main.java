4786d080c223fcd719c2c3ea8c01c39b8a7a9752
ge36wy <sejoklee@web.de>
2024-12-03 13:02:54 +0100

    Day 3 Part 2

--- AoCTag3/src/Main.java
+++ AoCTag3/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,13 +12,27 @@ public class Main {
             assert input != null;
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line;
+            StringBuilder text = new StringBuilder();
+            boolean enabled = true;
             int total = 0;
             while ((line = reader.readLine()) != null) {
-                Matcher matcher = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)").matcher(line);
-                while (matcher.find()){
-                    Matcher nums = Pattern.compile("\\d{1,3}").matcher(matcher.group());
+                text.append(line);
+            }
+            Matcher matcher = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)|don't|do").matcher(text.toString());
+            while (matcher.find()){
+                String next = matcher.group();
+                if(Objects.equals(next, "do")){
+                    enabled = true;
+                    continue;
+                }
+                if (Objects.equals(next, "don't")){
+                    enabled = false;
+                    continue;
+                }
+                if (enabled) {
+                    Matcher nums = Pattern.compile("\\d{1,3}").matcher(next);
                     int a = 1;
-                    while (nums.find()){
+                    while (nums.find()) {
                         a *= Integer.parseInt(nums.group());
                     }
                     total += a;

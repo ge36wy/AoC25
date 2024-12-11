40c22b0e10a48d6788c27c267e7bf64869cdd4d5
ge36wy <sejoklee@web.de>
2024-12-11 14:48:54 +0100

    Day 11 Part 2

--- AoCTag11/src/Main.java
+++ AoCTag11/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,32 +11,49 @@ public class Main {
             assert input != null;
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             String line;
-            long total;
-            ArrayList<Long> numbers = new ArrayList<>();
+            long total = 0;
+            HashMap<Long, Long> occurencies = new HashMap<>();
+            int blinks = 75;
             while ((line = reader.readLine()) != null) {
                 String[] nums = line.split(" ");
-                for (String s: nums) numbers.add(Long.parseLong(s));
+                for (String s: nums){
+                    occurencies.put(Long.parseLong(s), 1L);
+                }
             }
-            for (int i = 0; i < 25; i++){
-                ArrayList<Long> newNumbers = new ArrayList<>();
-                for (Long num: numbers){
-                    if (num == 0) {
-                        newNumbers.add(1L);
-                    } else if ((int)(Math.log10(num)+1) % 2 == 0) {
-                        String s = String.valueOf(num);
-                        newNumbers.add(Long.parseLong(s.substring(0, s.length() / 2)));
-                        newNumbers.add(Long.parseLong(s.substring(s.length() / 2)));
-                    } else {
-                        newNumbers.add(num * 2024);
+            for (int i = 0; i < blinks; i++){
+                System.out.println(i);
+                HashMap<Long, Long> newNumbersOcc = new HashMap<>();
+                for (Long num: occurencies.keySet()){
+                    Long o = occurencies.get(num);
+                    ArrayList<Long> next = nextStep(num);
+                    for (Long l: next){
+                        if (newNumbersOcc.containsKey(l)) {
+                            newNumbersOcc.put(l, newNumbersOcc.get(l) + o);
+                        } else {
+                            newNumbersOcc.put(l, o);
+                        }
                     }
                 }
-                numbers = newNumbers;
+                occurencies = newNumbersOcc;
             }
-            total = numbers.size();
+            for (Long l: occurencies.values()) total += l;
             System.out.println(total);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static ArrayList<Long> nextStep(long num){
+        ArrayList<Long> newNumbers = new ArrayList<>();
+        int digits = (int)(Math.log10(num)+1);
+        if (num == 0) {
+            newNumbers.add(1L);
+        } else if (digits % 2 == 0) {
+            newNumbers.add((long) (num / Math.pow(10, digits / 2)));
+            newNumbers.add((long) (num % Math.pow(10, digits / 2)));
+        } else {
+            newNumbers.add(num * 2024);
+        }
+        return newNumbers;
+    }
 }
